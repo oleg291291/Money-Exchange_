41c7c3ffdd8e45d85470d749136270b64e680150
oleg291291@mail.ru <oleg291291@mail.ru>
2018-02-21 20:13:39 +0300

    yo!

--- src/index.js
+++ src/index.js
@@ -3,9 +3,10 @@ module.exports = function makeExchange(currency) {
     // Your code goes here!
     // Return an object containing the minimum number of coins needed to make change
 
-    var h, q, d, n, p;
+       var h, q, d, n, p;
     var hC, qC, dC, nC, pC;
-    var result = []
+    var result = {};
+
     h = 50;
     q = 25;
     d = 10;
@@ -14,29 +15,34 @@ module.exports = function makeExchange(currency) {
 if (currency >= h){
     hC = Math.floor(currency / h);
     currency = currency % h;
+   result.H = hC;
 }
 if (currency >= q){
     qC = Math.floor(currency / q);
     currency = currency % q;
+  result.Q = qC;
+  
 }
 if (currency >= d){
     dC = Math.floor(currency / d);
     currency = currency % d;
+  result.D = dC;
+  
 }
+ 
 if (currency >= n){
     nC = Math.floor(currency / n);
     currency = currency % n;
+  result.N = nC;
+  
 }
 if (currency >= p){
     pC = Math.floor(currency / p);
     currency = currency % p;
+  result.P = pC;
+  
 }
-    console.log(hC);
-    console.log(qC);
-  console.log(dC);
-  console.log(nC);
-  console.log(pC);
-
-
-makeExchange(18);
+    
+  
+  
 }
